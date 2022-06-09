e6458ff63bfafad421bf8ffdd181ef67cbf341b4
Iuri de Araujo <iuri@iurix.com>
2022-06-09 17:11:52 -0500

    edited instalaltion scripts

--- install-openCV4-raspPi4.sh
+++ install-openCV4-raspPi4.sh
@@ -36,7 +36,7 @@ sudo apt-get -y remove x264 libx264-dev
 ## Install dependencies
 sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm
 sudo apt-get -y install git gfortran
-sudo apt-get -y install libjpeg8-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libjpeg-dev libjasper-dev libpng12-dev
 
 sudo apt-get -y install libtiff5-dev
 
@@ -75,21 +75,21 @@ sudo apt-get -y install python3-testresources
 
 cd $cwd
 # Install virtual environment
-python3 -m venv OpenCV-"$cvVersion"-py3
-echo "# Virtual Environment Wrapper" >> ~/.bashrc
-echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
-source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
+# python3 -m venv OpenCV-"$cvVersion"-py3
+# echo "# Virtual Environment Wrapper" >> ~/.bashrc
+# echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
+# source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
 #############
 
 
 ############ For Python 3 ############
 # now install python libraries within this virtual environment
-sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
-pip install numpy dlib
+pip3 install numpy dlib
 # quit virtual environment
-deactivate
+# deactivate
 
 
 git clone https://github.com/opencv/opencv.git
@@ -106,23 +106,7 @@ cd opencv
 mkdir build
 cd build
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-	-D CMAKE_INSTALL_PREFIX=$cwd/installation/OpenCV-"$cvVersion" \
-	-D INSTALL_C_EXAMPLES=ON \
-	-D INSTALL_PYTHON_EXAMPLES=ON \
-	-D ENABLE_NEON=ON \
-	-D ENABLE_VFPV3=ON \
-	-D WITH_OPENMP=ON \
-	-D WITH_TBB=ON \
-	-D BUILD_TBB=ON \
-	-D WITH_EIGEN=OFF \
-	-D WITH_V4L=ON \
-	-D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.7/site-packages \
-	-D WITH_QT=ON \
-	-D WITH_OPENGL=ON \
-	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-	-D BUILD_EXAMPLES=ON ..
-
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_PYTHON_EXAMPLES=OFF -D ENABLE_NEON=ON -D ENABLE_VFPV3=ON -D OPENCV_EXTRA_MODULES_PATH=/home/qonteo/opencv/opencv_contrib/modules -D BUILD_TESTS=OFF -D OPENCV_ENABLE_NONFREE=ON -D CMAKE_SHARED_LINKER_FLAGS=-latomic -D INSTALL_PYTHON_EXAMPLES=ON -D WITH_OPENMP=ON -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D BUILD_EXAMPLES=OFF ..
 
 make -j$(nproc)
 make install
