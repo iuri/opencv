4aa71f164e31336ca2589fcc8845d48d8ff044b1
Iuri de Araujo <iurix.com>
2022-06-24 13:54:43 -0500

    edited script for unicentro

--- detectFacesEyes.py
+++ detectFacesEyes.py
@@ -2,7 +2,11 @@ import numpy as np
 import cv2
 import sys, random, string, time
 import os, os.path
+import logging
 import config
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='/var/log/detectfaces.log',level=logging.INFO)
+
 faceCascade = cv2.CascadeClassifier('./haarcascade/haarcascade_frontalface_default.xml')
 # eyeCascade = cv2.CascadeClassifier('./haarcascade/haarcascade_eye.xml')
 eyeCascade = cv2.CascadeClassifier('./haarcascade/haarcascade_eye_tree_eyeglasses.xml')
@@ -21,7 +25,8 @@ if not os.path.isdir(path):
     os.umask(0)
     os.makedirs(path, mode=0o777, exist_ok=False)
 
-print("Connecting to camera ", config.camera_url) 
+#print("Connecting to camera ", config.camera_url) 
+logging.info('Connecting to camera: %s' % config.camera_url) 
 while True:
     ret, img = video_capture.read()
 
@@ -30,7 +35,6 @@ while True:
     
         faces = faceCascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
 
-        # print("[INFO] Found {0} Faces!".format(len(faces)))
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255,0, 0), 2)
             roi_gray = gray[y:y+h, x:x+w]
@@ -50,8 +54,10 @@ while True:
                     result_str = ''.join(random.choice(letters) for i in range(12))
                     status = cv2.imwrite("./images/"+result_str+".jpg", crop_img)
                     os.chmod("./images/"+result_str+".jpg", 0o777)
-                    print("Face detected")
-    # cv2.imshow("Window",img)
+                    logging.info('Face detected')
+                    # logging.info("Found {0} Faces!".format(len(faces)))
+
+    #cv2.imshow("Window",img)
                 
     # This breaks on 'q' key
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- watcher.py
+++ watcher.py
@@ -1,6 +1,10 @@
 
 import os, requests, time, socket
+import logging
 import config
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='/var/log/watcher.log',level=logging.INFO)
+
 image_path = './images'
 os.chdir(".")
 
@@ -22,13 +26,13 @@ while 1:
                         'Authorization': config.auth_token                     
                     }
                 
-                    # print("Sending file...", file_path)
+                    logging.info('Sending file... %s' % file_path)
                     # print("Destination", config.url)
                     with open(file_path,'rb') as fp:
                         file_dict = {'upload_file': (f, fp, 'multipart/form-data')}
                         response = requests.post(config.url, files=file_dict, headers=headers)
                         fp.close()
-                        # print("STATU", response.status_code)
+                        logging.info('STATUS %s' % response.status_code)
                         if response.status_code == 200 and response.text == 'ok':
                             os.remove(file_path)     
                             del headers
